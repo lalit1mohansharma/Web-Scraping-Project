d1e5bb1212f54edd0862b079f1dc809ead478e51
Lalit Mohan <33263263+lalit1mohansharma@users.noreply.github.com>
2025-05-15 23:00:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def main():
     chrome_options.add_argument("--no-sandbox")
 
     # Create credentials to access google spreadsheet
-    gc = gspread.service_account(filename='GOOGLESHEET JSON FILE PUT HERE')
-    sh = gc.open('Cameras - Daily Price Information')
-    print(sh)
+    # gc = gspread.service_account(filename='GOOGLESHEET JSON FILE PUT HERE')
+    # sh = gc.open('Cameras - Daily Price Information')
+    # print(sh)
 
     # Request moment site
     url = 'https://www.shopmoment.com/cameras'
@@ -45,187 +45,190 @@ def main():
     headers = ["Brands", "Name", "Special Message", "Current Price", "Retail Price", "No. of Reviews", "Status", "Link"]
     moment_df = pd.DataFrame(columns=headers)
 
-    browser = webdriver.Chrome(ChromeDriverManager().install() ,options=chrome_options)
-    browser.get(url)
-    time.sleep(1)
+    try:
+        browser = webdriver.Chrome(ChromeDriverManager().install() ,options=chrome_options)
+        browser.get(url)
+        time.sleep(1)
+    except Exception as e:
+            print(f"\nChrome driver initialization failed: {str(e)}")
 
     scroll = True
 
-    while scroll == True:
-        try:
-            time.sleep(3)
-            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            action = ActionChains(browser)
-            load_more = browser.find_element_by_xpath('//*[@id="shopGridContainer"]/div[2]/div[2]/button')
-            browser.execute_script("arguments[0].scrollIntoView();", load_more)
-            action.move_to_element(load_more)
-            action.click()
-            action.perform()
-            print("Action clicked!")
-        except NoSuchElementException:
-            scroll = False
-            print("No more button!")
+    # while scroll == True:
+    #     try:
+    #         time.sleep(3)
+    #         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #         action = ActionChains(browser)
+    #         load_more = browser.find_element_by_xpath('//*[@id="shopGridContainer"]/div[2]/div[2]/button')
+    #         browser.execute_script("arguments[0].scrollIntoView();", load_more)
+    #         action.move_to_element(load_more)
+    #         action.click()
+    #         action.perform()
+    #         print("Action clicked!")
+    #     except NoSuchElementException:
+    #         scroll = False
+    #         print("No more button!")
 
-    time.sleep(5)
-    all_products = browser.find_elements_by_css_selector('article.product-card-display')
+    # time.sleep(5)
+    # all_products = browser.find_elements_by_css_selector('article.product-card-display')
 
-    print(f"Number of products: {len(all_products)}")
+    # print(f"Number of products: {len(all_products)}")
 
-    home = 'https://www.shopmoment.com'
+    # home = 'https://www.shopmoment.com'
 
-    for i in range(len(all_products)):
-        all_products_html = all_products[i].get_attribute('outerHTML')
-        product = BeautifulSoup(all_products_html, "html.parser")
+    # for i in range(len(all_products)):
+    #     all_products_html = all_products[i].get_attribute('outerHTML')
+    #     product = BeautifulSoup(all_products_html, "html.parser")
 
-        brands = product.find("header", {"class": "product-card-display__bottom__header"}).find("a").text.strip()
-        title = product.find("header", {"class": "product-card-display__bottom__header"}).find("h2", {"class": "product-card-title__title"}).text.strip()
-        reviews = product.find("footer", {"class": "product-card-display__bottom__footer"}).find("span", {"class": "product-card-rating__total-reviews"})
-        current_price = product.find("footer", {"class": "product-card-display__bottom__footer"}).find("span", {"class": "product-card-pricing__price"}).text.strip().lstrip("$").rstrip("+").replace(",", "")
-        retail_price = product.find("footer", {"class": "product-card-display__bottom__footer"}).find("del", {"class": "product-card-pricing__price--retail"})
-        special_message = product.find("div", {"class": "product-card-display__top"}).find("figure", {"class": "product-card-notifier"})
-        availability = product.find("div", {"class": "product-card-display__top"}).select(".product-card-action")[0].text.strip()
-        link = product.find("div", {"class": "product-card-display__top"}).find("a").get("href")
+    #     brands = product.find("header", {"class": "product-card-display__bottom__header"}).find("a").text.strip()
+    #     title = product.find("header", {"class": "product-card-display__bottom__header"}).find("h2", {"class": "product-card-title__title"}).text.strip()
+    #     reviews = product.find("footer", {"class": "product-card-display__bottom__footer"}).find("span", {"class": "product-card-rating__total-reviews"})
+    #     current_price = product.find("footer", {"class": "product-card-display__bottom__footer"}).find("span", {"class": "product-card-pricing__price"}).text.strip().lstrip("$").rstrip("+").replace(",", "")
+    #     retail_price = product.find("footer", {"class": "product-card-display__bottom__footer"}).find("del", {"class": "product-card-pricing__price--retail"})
+    #     special_message = product.find("div", {"class": "product-card-display__top"}).find("figure", {"class": "product-card-notifier"})
+    #     availability = product.find("div", {"class": "product-card-display__top"}).select(".product-card-action")[0].text.strip()
+    #     link = product.find("div", {"class": "product-card-display__top"}).find("a").get("href")
 
-        if reviews == None:
-            reviews = "0"
-        else:
-            reviews = reviews.text.strip().lstrip("(").rstrip(")")
+    #     if reviews == None:
+    #         reviews = "0"
+    #     else:
+    #         reviews = reviews.text.strip().lstrip("(").rstrip(")")
 
-        if special_message == None:
-            special_message = " "
-        else:
-            special_message = special_message.text.strip()
+    #     if special_message == None:
+    #         special_message = " "
+    #     else:
+    #         special_message = special_message.text.strip()
 
-        if retail_price == None:
-            retail_price = " "
-        else:
-            retail_price = retail_price.text.strip().lstrip("$").replace(",", "")
+    #     if retail_price == None:
+    #         retail_price = " "
+    #     else:
+    #         retail_price = retail_price.text.strip().lstrip("$").replace(",", "")
 
-        if availability == "Add to cart":
-            availability = "Available"
+    #     if availability == "Add to cart":
+    #         availability = "Available"
 
-        full_link = home + link
+    #     full_link = home + link
 
-        current_product = {"Brands": brands, "Name": title, "Special Message": special_message, "Current Price": current_price, "Retail Price": retail_price, "No. of Reviews": reviews, "Status": availability, "Link": full_link}
-        moment_df = moment_df.append(current_product, ignore_index=True)
+    #     current_product = {"Brands": brands, "Name": title, "Special Message": special_message, "Current Price": current_price, "Retail Price": retail_price, "No. of Reviews": reviews, "Status": availability, "Link": full_link}
+    #     moment_df = moment_df.append(current_product, ignore_index=True)
 
-    time.sleep(5)
-    browser.close()
+    # time.sleep(5)
+    # browser.close()
 
-    moment_df.sort_values(by=["Brands", "Name"], inplace=True, ignore_index=True)
-    moment_df["Current Price"] = moment_df["Current Price"].astype("float64")
-    moment_df["No. of Reviews"] = moment_df["No. of Reviews"].astype("int")
-
-
-    ############ Compare Prices and Saving to google spreadsheet #################
-
-    year, month, day = str(date.today()).split("-")
-
-    today = date.today()
-    yesterday = today - timedelta(days=1)
-    y_year, y_month, y_day = str(yesterday).split("-")
-
-    try:
-        yesterday_worksheet = sh.worksheet(f"{y_day}{y_month}{y_year}")
-        yesterday_df = pd.DataFrame(yesterday_worksheet.get_all_records())
-        yesterday_df = yesterday_df.rename({"Current Price": "Yesterday Price"}, axis=1)
-
-        merged = moment_df.merge(yesterday_df, how="left",
-                                left_on=["Name", "Current Price"],
-                                right_on=["Name", "Yesterday Price"])
+    # moment_df.sort_values(by=["Brands", "Name"], inplace=True, ignore_index=True)
+    # moment_df["Current Price"] = moment_df["Current Price"].astype("float64")
+    # moment_df["No. of Reviews"] = moment_df["No. of Reviews"].astype("int")
 
 
-        differences = merged["Current Price"] - merged["Yesterday Price"]
-        differences.fillna("New", inplace=True)
+    # ############ Compare Prices and Saving to google spreadsheet #################
 
-        moment_df.insert(5, "Price Differences", differences)
+    # year, month, day = str(date.today()).split("-")
 
-        worksheet_name = day + month + year
-        today_worksheet = sh.add_worksheet(title=worksheet_name, rows="100", cols="20")
-        today_worksheet.update([moment_df.columns.values.tolist()] + moment_df.values.tolist())
-    except gspread.exceptions.WorksheetNotFound:
-        worksheet_name = day + month + year
-        today_worksheet = sh.add_worksheet(title=worksheet_name, rows="100", cols="20")
+    # today = date.today()
+    # yesterday = today - timedelta(days=1)
+    # y_year, y_month, y_day = str(yesterday).split("-")
 
-        today_worksheet.update([moment_df.columns.values.tolist()] + moment_df.values.tolist())
+    # try:
+    #     yesterday_worksheet = sh.worksheet(f"{y_day}{y_month}{y_year}")
+    #     yesterday_df = pd.DataFrame(yesterday_worksheet.get_all_records())
+    #     yesterday_df = yesterday_df.rename({"Current Price": "Yesterday Price"}, axis=1)
 
-    print("Successfully updated the extracted data to google spreadsheet!")
+    #     merged = moment_df.merge(yesterday_df, how="left",
+    #                             left_on=["Name", "Current Price"],
+    #                             right_on=["Name", "Yesterday Price"])
 
 
-    ################### Telegram API #####################
+    #     differences = merged["Current Price"] - merged["Yesterday Price"]
+    #     differences.fillna("New", inplace=True)
 
-    telegram_url = 'https://api.telegram.org/bot'
-    token = 'TELEGRAM TOKEN PUT HERE'
-    method = '/getMe'
+    #     moment_df.insert(5, "Price Differences", differences)
 
-    full_url = telegram_url + token + method
-    bot = requests.get(full_url)
-    print(f"Telegram bot status code: {bot.status_code}")
+    #     worksheet_name = day + month + year
+    #     today_worksheet = sh.add_worksheet(title=worksheet_name, rows="100", cols="20")
+    #     today_worksheet.update([moment_df.columns.values.tolist()] + moment_df.values.tolist())
+    # except gspread.exceptions.WorksheetNotFound:
+    #     worksheet_name = day + month + year
+    #     today_worksheet = sh.add_worksheet(title=worksheet_name, rows="100", cols="20")
 
-    test_response = requests.get(f"{telegram_url}{token}/getUpdates")
+    #     today_worksheet.update([moment_df.columns.values.tolist()] + moment_df.values.tolist())
 
-    my_id = 'YOUR TELEGRAM ID'
-    cell_f1 = today_worksheet.acell("F1").value
-    print(f"Cell F1: {cell_f1}")
+    # print("Successfully updated the extracted data to google spreadsheet!")
 
-    new_product_arr = []
-    price_increase_arr = []
-    price_decrease_arr = []
-    if cell_f1 == "Price Differences":
-        for v in today_worksheet.get_all_values()[1:]:
-            price_difference = v[5]
-            if price_difference == '0':
-                continue
-            elif price_difference == 'New':
-                new_product_arr.append(v)
-            elif int(price_difference) > 0:
-                price_increase_arr.append(v)
-            elif int(price_difference) < 0:
-                price_decrease_arr.append(v)
 
-    text = ''
-    text += f"*Date: {day}/{month}/{year}*\n\n"
+    # ################### Telegram API #####################
 
-    if len(new_product_arr) != 0:
-        text += f"_*__New Product__*_\n"
-        for p in new_product_arr:
-            name = p[1].replace("-", "\-").replace("+", "\+").replace(".", "\.")
-            price = p[3].replace(".", "\.")
-            text += f"Brands: {p[0]}\nName: {name}\nPrice: USD {price}\n"
-            if p[2] != ' ':
-                t = p[2].replace("!", "\!").replace(".", "\.")
-                text += f"Special: {t}\n"
-            link = p[8].replace(".", "\.").replace("-", "\-")
-            text += f"Link: {link}\n\n"
+    # telegram_url = 'https://api.telegram.org/bot'
+    # token = 'TELEGRAM TOKEN PUT HERE'
+    # method = '/getMe'
 
-    if len(price_decrease_arr) != 0:
-        text += f"_*__Price Drop__*_\n"
-        for p in price_decrease_arr:
-            name = p[1].replace("-", "\-").replace("+", "\+").replace(".", "\.")
-            price = p[3].replace(".", "\.")
-            text += f"Brands: {p[0]}\nName: {name}\nPrice: USD {price}\n"
-            if p[2] != ' ':
-                t = p[2].replace("!", "\!").replace(".", "\.")
-                text += f"Special: {t}\n"
-            link = p[8].replace(".", "\.").replace("-", "\-")
-            text += f"Link: {link}\n\n"
+    # full_url = telegram_url + token + method
+    # bot = requests.get(full_url)
+    # print(f"Telegram bot status code: {bot.status_code}")
 
-    if len(price_increase_arr) != 0:
-        text += f"_*__Price Increase__*_\n"
-        for p in price_increase_arr:
-            name = p[1].replace("-", "\-").replace("+", "\+").replace(".", "\.")
-            price = p[3].replace(".", "\.")
-            text += f"Brands: {p[0]}\nName: {name}\nPrice: USD {price}\n"
-            if p[2] != ' ':
-                t = p[2].replace("!", "\!").replace(".", "\.")
-                text += f"Special: {t}\n"
-            link = p[8].replace(".", "\.").replace("-", "\-")
-            text += f"Link: {link}\n\n"
+    # test_response = requests.get(f"{telegram_url}{token}/getUpdates")
 
-    if len(new_product_arr) == 0 and len(price_decrease_arr) == 0 and len(price_increase_arr) == 0:
-        text += "There is no changes\.\nVisit https://www\.shopmoment\.com to explore more products\.\n"
+    # my_id = 'YOUR TELEGRAM ID'
+    # cell_f1 = today_worksheet.acell("F1").value
+    # print(f"Cell F1: {cell_f1}")
 
-    send_message(my_id, text, telegram_url, token)
+    # new_product_arr = []
+    # price_increase_arr = []
+    # price_decrease_arr = []
+    # if cell_f1 == "Price Differences":
+    #     for v in today_worksheet.get_all_values()[1:]:
+    #         price_difference = v[5]
+    #         if price_difference == '0':
+    #             continue
+    #         elif price_difference == 'New':
+    #             new_product_arr.append(v)
+    #         elif int(price_difference) > 0:
+    #             price_increase_arr.append(v)
+    #         elif int(price_difference) < 0:
+    #             price_decrease_arr.append(v)
+
+    # text = ''
+    # text += f"*Date: {day}/{month}/{year}*\n\n"
+
+    # if len(new_product_arr) != 0:
+    #     text += f"_*__New Product__*_\n"
+    #     for p in new_product_arr:
+    #         name = p[1].replace("-", "\-").replace("+", "\+").replace(".", "\.")
+    #         price = p[3].replace(".", "\.")
+    #         text += f"Brands: {p[0]}\nName: {name}\nPrice: USD {price}\n"
+    #         if p[2] != ' ':
+    #             t = p[2].replace("!", "\!").replace(".", "\.")
+    #             text += f"Special: {t}\n"
+    #         link = p[8].replace(".", "\.").replace("-", "\-")
+    #         text += f"Link: {link}\n\n"
+
+    # if len(price_decrease_arr) != 0:
+    #     text += f"_*__Price Drop__*_\n"
+    #     for p in price_decrease_arr:
+    #         name = p[1].replace("-", "\-").replace("+", "\+").replace(".", "\.")
+    #         price = p[3].replace(".", "\.")
+    #         text += f"Brands: {p[0]}\nName: {name}\nPrice: USD {price}\n"
+    #         if p[2] != ' ':
+    #             t = p[2].replace("!", "\!").replace(".", "\.")
+    #             text += f"Special: {t}\n"
+    #         link = p[8].replace(".", "\.").replace("-", "\-")
+    #         text += f"Link: {link}\n\n"
+
+    # if len(price_increase_arr) != 0:
+    #     text += f"_*__Price Increase__*_\n"
+    #     for p in price_increase_arr:
+    #         name = p[1].replace("-", "\-").replace("+", "\+").replace(".", "\.")
+    #         price = p[3].replace(".", "\.")
+    #         text += f"Brands: {p[0]}\nName: {name}\nPrice: USD {price}\n"
+    #         if p[2] != ' ':
+    #             t = p[2].replace("!", "\!").replace(".", "\.")
+    #             text += f"Special: {t}\n"
+    #         link = p[8].replace(".", "\.").replace("-", "\-")
+    #         text += f"Link: {link}\n\n"
+
+    # if len(new_product_arr) == 0 and len(price_decrease_arr) == 0 and len(price_increase_arr) == 0:
+    #     text += "There is no changes\.\nVisit https://www\.shopmoment\.com to explore more products\.\n"
+
+    # send_message(my_id, text, telegram_url, token)
     return "Done!"
 
 if __name__ == "__main__":
